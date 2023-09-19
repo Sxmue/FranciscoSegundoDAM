8219eae1024e5c5eb48aecc55b5527968a18b558
Sxmue <samu_5@hotmail.es>
2023-09-19 23:23:46 +0200

    Prueba inicial

---
+++ HelloWorldDeVerdad/src/main/java/prueba/Inicial/Main.java
@@ -0,0 +1,9 @@
+package prueba.Inicial;
+
+public class Main {
+    public static void main(String[] args) {
+        NumerosPrimos juego = new NumerosPrimos();
+        juego.start();
+    }
+
+}
---
+++ HelloWorldDeVerdad/src/main/java/prueba/Inicial/NumerosPrimos.java
@@ -0,0 +1,81 @@
+package prueba.Inicial;
+
+import java.util.Scanner;
+
+public class NumerosPrimos {
+
+    //Contador usado para contar divisores
+    private int contador = 0;
+
+    //Contador para saber cuantos numeros primos llevamos
+    private int contadorPrim = 0;
+
+    //Variable que almacena el principio del intervalo
+    private int start;
+
+    //Variable que almacena el final del intervalo
+    private int fin;
+
+    //Variable auxiliar para las comprobaciones
+    private int auxiliar = 0;
+
+    //boolean para comprobar si es la primera partida
+    private boolean ronda;
+
+
+    //Metodo que se encarga de comprobar los divisores de cada numero del intervalo
+    public void division(int x) {
+        for (auxiliar = fin; auxiliar >= 1; auxiliar--) {
+            if (x % auxiliar == 0) {
+                contador++;
+            }
+        }
+        if (contador == 2) {
+            System.out.println(x);
+            contadorPrim++;
+        }
+        contador = 0;
+    }
+
+
+    //Metodo que recorre el intervalo y finaliza el programa en caso de querer terminar
+    public void recorrido() {
+            System.out.println("Buscando primos...");
+            for (int x = start; x <= fin; x++) {
+                division(x);
+            }
+
+            System.out.println("Se han encontrado " + contadorPrim + " numeros primos en el intervalo");
+
+
+    }
+
+
+    //Metodo que se encarga de pedir el intervalo al usuario
+    public void start() {
+        try (Scanner sc = new Scanner(System.in)) {
+            start = 1;
+            while( start > 0) {
+                    System.out.println("Escriba el numero inicial (Pulsa 0 para salir):");
+                    start = sc.nextInt();
+
+                if(start==0) {
+                    System.out.println("Fin del programa");
+                }else {
+
+                    System.out.println("Escriba el numero final:");
+                    fin = sc.nextInt();
+                    recorrido();
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
+
+
+
